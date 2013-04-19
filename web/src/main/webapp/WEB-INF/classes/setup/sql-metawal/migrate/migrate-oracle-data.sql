2b04858be0bbee4eb10bab71888c5581020215a5
François Prunayre <fxprunayre@gmail.com>
2013-04-19 10:11:35 +0200

    We're now in 2.11.x dev line.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/migrate-oracle-data.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/migrate-oracle-data.sql
@@ -200,6 +200,6 @@ UPDATE metadata SET data = replace(data,
 
 
 -- Version update
-UPDATE Settings SET value='2.9.0' WHERE name='version';
+UPDATE Settings SET value='2.11.0' WHERE name='version';
 UPDATE Settings SET value='0' WHERE name='subVersion';
 UPDATE Settings SET value='2c0a95c0-9536-11e2-9e96-0800200c9a66' WHERE name='siteId';
